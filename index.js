e34dd8418542a13624556d6324619dfa0f0ce78b
Carlos Lugo <calugo71@gmail.com>
2022-08-02 14:52:03 -0400

    renders cards, dispalys ingrediants, plays mp3

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-const drinkCardTemplate=document.querySelector("[data-drink-template]")
-const userCardContainer=document.querySelector("[data-drink-cards-container]")
-const searchInput=document.querySelector("[data-search]")
+const submitBtn=document.getElementById('search');
+const searchParam=document.getElementById('search-text');
+const drinkCards=document.querySelector(".drink-cards");
 
-let drinks = []
+submitBtn.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    console.log(searchParam.value)
+});
 
+function fetchSearch(){
+fetch('https://api.api-ninjas.com/v1/cocktail?name='+ searchParam.value, {
+    headers: { 'X-Api-Key': 'K94J8aIYDUHZ4VyJ4vqXqw==YNi6rwL7ZYxX0LpE'}})
+    .then(res=>res.json())
+    .then(data=> {
+        data.forEach(datas => {
+            renderDrinkCards(datas)
+        })
+    });
+};
 
-searchInput.addEventListener("input", (e) => {
-    const value=e.target.value
-    console.log(value)
-})
-
-fetch('https:www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka')
-.then(res=>res.json())
-.then(data=> {
-    data.drinks.forEach(drinks => {
-    const card=drinkCardTemplate.content.cloneNode(true).children[0];
-    const header = card.querySelector("[data-header");
-    const thumb = card.querySelector("[data-image]");
-    header.textContent=drinks.strDrink;
-    thumb.src=drinks.strDrinkThumb;
-    userCardContainer.append(card);
-})});
+function renderDrinkCards(datas){
+    // let audio= new Audio("Downloads/J Dilla - No One's Home_pWkzofZ1YKQ_youtube.mp3")
+    let siteHeader=document.querySelector(".top-header")
+    let cardBody = document.createElement('div')
+    let cardText=document.createElement('p')
+    let cardTitle = document.createElement('h5')
+    let cardSpirit=document.createElement('button')
+    cardBody.className = "card-body"
+    cardTitle.className = "card-title"
+    cardSpirit.className="spirit-btn"
+    cardSpirit.innerText="What's Your Spirit?!"
+    cardTitle.innerText = datas.name
+    cardText.className="card-text"
+    cardText.innerText= datas.ingredients
+    cardBody.append(cardTitle, cardText, cardSpirit)
+    drinkCards.append(cardBody)
+    cardBody.addEventListener('mouseover', (e) => {
+        siteHeader.innerText=datas.instructions
+    })
+    cardSpirit.addEventListener('click', (e) => {
+        document.getElementById('audio1').play();
+    })
+}
