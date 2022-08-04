57b3923fa7d0a7089f69d2e5fab9ce16811b36b8
Carlos Lugo <calugo71@gmail.com>
2022-08-04 16:50:44 -0400

    added more music

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # phase-1-project
+
+For many users, their drink choice of preference is as vital to their personality as their sense of style, musical tastes, etc. 
+
+For this web page, we have devised a quiz to find out 'What's Your Spirit?!" based on what drink captures the user's eye. 
+
+Firstly, we had to find an appropriate API. The Cocktail DB API had many options, but would not provide the additional information in each fetch request that we wanted to render for our cards. Ultimately, we went with a cockatil API provided by API Ninja, as it would provide ingrediants and instructions in each returned object. 
+
+Once we had the right API for our page, we tested the the results of our GET request in a form submisiion. The form would search for drinks that included an ingrediant or dink name, and return single or multiple drinks that met the parameter. Now that we were receiving the data with the compnents that we needed, in the fashion that we wanted, we wanted to render out waht we were getting following the wireframe found in WireFrameExample.png found in this file. 
+
+Now that we had our cards rendered into the body of our page, the name and/or ingrediants of each drink is prominently displayed onthe cards face. Wichever one captures the user's eye, they will be able to see instructions on how to make each drink by hovering their mouse over the card. 
+
+Next, when a user picks a drink that most speaks to them, they will be able to click a button that will play a locally hosted mp3 file that correlates to their drink search. 
+
+Finally, we wanted to add something so that the page isn't too bare when first rendered. In the spirit of the project, we picked a gif of the bar scene from 'The Other Guys', which goes with our drink-themed page, and adds a few laughs. 
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const submitBtn=document.getElementById('search');
 const searchParam=document.getElementById('search-text');
 const drinkCards=document.querySelector(".drink-cards");
+const aha="ProjectAudio/aha.mp3";
+const dancingQueen="ProjectAudio/dancingQueen.mp3" 
 
 submitBtn.addEventListener('submit', (e) =>{
     e.preventDefault();
     console.log(searchParam.value)
-});
+    drinkCards.innerHTML='';
+}); 
 
 function fetchSearch(){
-fetch('https://api.api-ninjas.com/v1/cocktail?name='+ searchParam.value, {
-    headers: { 'X-Api-Key': 'K94J8aIYDUHZ4VyJ4vqXqw==YNi6rwL7ZYxX0LpE'}})
+    fetch('https://api.api-ninjas.com/v1/cocktail?name='+ searchParam.value, {
+        headers: { 'X-Api-Key': 'K94J8aIYDUHZ4VyJ4vqXqw==YNi6rwL7ZYxX0LpE'}})
     .then(res=>res.json())
     .then(data=> {
         data.forEach(datas => {
@@ -19,25 +22,144 @@ fetch('https://api.api-ninjas.com/v1/cocktail?name='+ searchParam.value, {
 };
 
 function renderDrinkCards(datas){
-    // let audio= new Audio("Downloads/J Dilla - No One's Home_pWkzofZ1YKQ_youtube.mp3")
     let siteHeader=document.querySelector(".top-header")
-    let cardBody = document.createElement('div')
+    let cardBody=document.createElement('div')
     let cardText=document.createElement('p')
-    let cardTitle = document.createElement('h5')
+    let cardTitle=document.createElement('h5')
     let cardSpirit=document.createElement('button')
-    cardBody.className = "card-body"
-    cardTitle.className = "card-title"
+    cardBody.className="card-body"
+    cardTitle.className="card-title"
     cardSpirit.className="spirit-btn"
     cardSpirit.innerText="What's Your Spirit?!"
-    cardTitle.innerText = datas.name
+    cardTitle.innerText=datas.name
     cardText.className="card-text"
-    cardText.innerText= datas.ingredients
+    cardText.innerText=datas.ingredients
     cardBody.append(cardTitle, cardText, cardSpirit)
     drinkCards.append(cardBody)
     cardBody.addEventListener('mouseover', (e) => {
         siteHeader.innerText=datas.instructions
     })
     cardSpirit.addEventListener('click', (e) => {
-        document.getElementById('audio1').play();
-    })
-}
+        if(searchParam.value=='champagne'){
+            document.getElementById('dancingQueen').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('dancingQueen').pause();
+            })
+    }
+        else if(searchParam.value=='martini'){
+            document.getElementById('aha').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('aha').pause();
+            })
+        }
+        else if(searchParam.value=='rum'){
+            document.getElementById('rum').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('rum').pause();
+            })
+        }
+        else if(searchParam.value=='tequila'){
+            document.getElementById('tequila').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('tequila').pause();
+            })
+        }
+        else if(searchParam.value=='sugar'){
+            document.getElementById('sugar').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('sugar').pause();
+            })
+        }
+        else if(searchParam.value=='beer'){
+            document.getElementById('beer').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('beer').pause();
+            })
+        }
+        else if(searchParam.value=='vodka'){
+            document.getElementById('vodka').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('vodka').pause();
+            })
+        }
+        else if(searchParam.value=='coconut'){
+            document.getElementById('coconut').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('coconut').pause();
+            })
+        }
+        else if(searchParam.value=='coke'){
+            document.getElementById('coke').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('coke').pause();
+            })
+        }
+        else if(searchParam.value=='mint'){
+            document.getElementById('mint').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('mint').pause();
+            })
+        }
+        else if(searchParam.value=='pop'){
+            document.getElementById('pop').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('pop').pause();
+            })
+        }
+        else if(searchParam.value=='bourbon'){
+            document.getElementById('bourbon').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('bourbon').pause();
+            })
+        }
+        else if(searchParam.value=='scotch'){
+            document.getElementById('scotch').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('scotch').pause();
+            })
+        }
+        else if(searchParam.value=='wine'){
+            document.getElementById('wine').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('wine').pause();
+            })
+        }
+        else if(searchParam.value=='brandy'){
+            document.getElementById('brandy').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('brandy').pause();
+            })
+        }
+        else if(searchParam.value=='margarita'){
+            document.getElementById('margarita').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('margarita').pause();
+            })
+        }
+        else if(searchParam.value=='whiskey'){
+            document.getElementById('whiskey').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('whiskey').pause();
+            })
+        }
+        else if(searchParam.value=='irish'){
+            document.getElementById('irish').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('irish').pause();
+            })
+        }
+        else if(searchParam.value=='gin'){
+            document.getElementById('gin').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('gin').pause();
+            })
+        }
+        else if(searchParam.value=='french'){
+            document.getElementById('french').play();
+            cardSpirit.addEventListener('click', (e) =>{
+                document.getElementById('french').pause();
+            })
+        }
+    });
+
+};
